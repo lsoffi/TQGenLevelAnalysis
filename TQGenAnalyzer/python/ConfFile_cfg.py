9e85aff38d2c374b43603e46a9492161427b7353
livia <livia.soffi@cern.ch>
2021-03-31 11:58:54 +0200

    fixing a py config

--- TQGenAnalyzer/python/ConfFile_cfg.py
+++ TQGenAnalyzer/python/ConfFile_cfg.py
@@ -7,7 +7,7 @@ process = cms.Process("GenAnalysis")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(100) )
 
 process.source = cms.Source("PoolSource",
                                 # replace 'myfile.root' with the source file you want to use
